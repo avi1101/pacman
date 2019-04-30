83c15b2de5561095906b9ac56e9a70938eb7f25c
elad <eladc5492@gmail.com>
2019-04-30 15:13:40 +0300

    testing commit

--- scripts/registerLogic.js
+++ scripts/registerLogic.js
@@ -1,28 +1,24 @@
-// function foo(){
-//     alert("works");
-//     var x = document.getElementById("email").value;
-//     if(x == "a")
-//         return true;
-//     return false;
-// }
+function foo(){
+    alert("works");
+    var x = document.getElementById("email").value;
+    if(x == "a")
+        return true;
+    return false;
+}
 
 $( document ).ready(function() {
-
-    $( "#reg_btn" ).click(function() {
-        $('#register').validate({  // initialize the plugin on your form.
-            // rules, options, and/or callback functions
-        });
-
-        // trigger validity test of any element using the .valid() method.
-        $('#email').valid();
-        $('#psw').valid();
-        $('#psw-repeat').valid();
-
-        // the .valid() method also returns a boolean...
-        if ($('#register').valid()) {
-            // something to do if form is valid
-
-        }
-        alert("works");
+    $('#reg_btn').on('click', function() {
+             var username = $('#email').val();
+             var password = $('#psw').val();
+             var firstname = $('#firstname').val();
+             var lastname = $('#lastname').val();
+             var date = $('#date').val();
+             if(!/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/.test(username))
+             {
+                 $('#warnings').text("Email not valie");
+                 return;
+             }
+             alert("workds");
     });
+
 });
